0f43e9de314e34654f8f20569a93feaed1d44990
astyler <astyler@gmail.com>
2015-07-07 22:41:47 -0400

    fixed bearing calculation, but now in radians

--- chargecarprize/javatestsuite/src/org/chargecar/prize/util/TripBuilder2.java
+++ chargecarprize/javatestsuite/src/org/chargecar/prize/util/TripBuilder2.java
@@ -228,12 +228,17 @@ public class TripBuilder2 {
     
     public static double Haversine(double lat1, double lon1, double lat2,
 	    double lon2) {
+	lat1 = Math.toRadians(lat1);
+	lat2 = Math.toRadians(lat2);
+	lon1 = Math.toRadians(lon1);
+	lon2 = Math.toRadians(lon2);
+	
 	double R = 6371000; // earth radius, metres
-	double dLat = Math.toRadians(lat2 - lat1);
-	double dLon = Math.toRadians(lon2 - lon1);
+	double dLat = lat2 - lat1;
+	double dLon = lon2 - lon1;
 	double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-	+ Math.cos(Math.toRadians(lat1))
-	* Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+	+ Math.cos(lat1)
+	* Math.cos(lat2) * Math.sin(dLon / 2)
 	* Math.sin(dLon / 2);
 	double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 	double d = R * c;
@@ -242,10 +247,13 @@ public class TripBuilder2 {
     
     public static double getBearing(double lat1, double lon1, double lat2,
 	    double lon2) {
-	double y = Math.sin(lat2-lat1) * Math.cos(lat2);
-	double x = Math.cos(lat1)*Math.sin(lat2) -
-	        Math.sin(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1);
-	return (Math.toDegrees(Math.atan2(y, x))+360)%360;
+	lat1 = Math.toRadians(lat1);
+	lat2 = Math.toRadians(lat2);
+	lon1 = Math.toRadians(lon1);
+	lon2 = Math.toRadians(lon2);
+	
+
+	return Math.atan2(Math.cos(lat1)*Math.sin(lat2)-Math.sin(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1), Math.sin(lon2-lon1)*Math.cos(lat2)); 
     }
     
 }
